f3d595b2c2498f9f2d4a757bdfc4f7ed23c354a3
phryk <phryk@commander1024.de>
2015-06-14 05:24:22 +0200

    form generation/rendering coming along

--- poobrains/db.py
+++ poobrains/db.py
@@ -2,12 +2,34 @@
 
 from math import ceil, floor
 from re import match
-from flask import abort, url_for, current_app, request
+from collections import OrderedDict
+from flask import abort, render_template, url_for, current_app, request
 from werkzeug.routing import BuildError
 import peewee
 from .rendering import ChildAware, Renderable, Menu
 
 db_proxy = peewee.Proxy()
+
+
+class ValidationError(ValueError):
+
+    model = None
+    field = None
+    value = None
+
+    def __init__(self, model, field, value):
+
+        super(ValidationError, self).__init__()
+
+        self.model = model
+        self.field = field
+        self.value = value
+
+        self.message = "Tried assigning invalid value to %s.%s: %s" % (self.model.__name__, self.field.name, str(value))
+
+
+    def __str__(self):
+        return "<%s: %s>" % (self.__class__.__name__, self.message)
 
 
 class BaseModel(peewee.Model, ChildAware):
@@ -36,7 +58,7 @@ class Storable(BaseModel, Renderable):
     def __setattr__(self, name, value):
 
         if name == 'name' and not match('^[a-zA-ZäÄöÖüÜ_\-]*$', value):
-            raise ValueError("Invalid value for name: %s" % (value,))
+            raise ValidationError(self.__class__, getattr(self.__class__, name), value)
 
         super(Storable, self).__setattr__(name, value)
 
@@ -51,14 +73,23 @@ class Storable(BaseModel, Renderable):
             else:
                 instance = cls.get(cls.name == id_or_name)
 
-        except cls.DoesNotExist as e:
+        except cls.DoesNotExist:
             abort(404, "It is pitch black. You are likely to be eaten by a grue.")
 
-        except peewee.OperationalError as e:
+        except peewee.OperationalError:
             if current_app.debug:
                 raise
 
             abort(500, "Somebody set up us the bomb.")
+
+        except ValidationError as e:
+
+            current_app.logger.error("Database integrity impaired. Invalid data in %s.%s: %s" % (cls.__name__, e.field.name, e.value))
+
+            if current_app.debug:
+                raise
+
+            abort(500, "VERY ERROR. SUCH DISGRACE. MANY SORRY.")
 
         return instance
 
@@ -70,6 +101,17 @@ class Storable(BaseModel, Renderable):
 
     def instance_url(self, mode=None):
         return current_app.get_url(self.__class__, id_or_name=self.name, mode=mode)
+
+
+    def form(self):
+        form = Form()
+
+        fields = self.__class__._meta.get_fields()
+
+        for field in fields:
+            form.add_field(field.name, field.__class__.__name__.lower(), getattr(self, field.name))
+
+        return form
 
 
     def render(self, mode='full'):
@@ -129,7 +171,7 @@ class Listing(Renderable):
             except StopIteration:
                 iteration_done = True
 
-            except ValueError as e:
+            except ValidationError as e:
                 current_app.logger.error(e.message)
 
         # Build pagination if matching endpoint and enough rows exist
@@ -155,37 +197,51 @@ class Listing(Renderable):
                 self.pagination = False
 
         except BuildError as e:
-            current_app.logger.debug('Pagination navigation could not be built. This might be fixable with more magic.')
+            current_app.logger.error('Pagination navigation could not be built. This might be fixable with more magic.')
             self.pagination = False
 
 
 
 class Form(Renderable):
 
+    title = None
+    fields = None
     rendered = None
 
-    def __init__(self, model_or_instance):
-
+    def __init__(self, title=''):
+        
+        self.title = title
+        self.fields = OrderedDict()
         self.render_reset()
-
-        if isinstance(BaseModel, model_or_instance):
-            model = model_or_instance.__class__
-            instance = model_or_instance
-
-        else:
-            model = model_or_instance
-            instance = None
 
         #TODO: add Storable.form() for class and instance
 
+
+    def add_field(self, name, field_type, value=None):
+        self.fields[name] = (field_type, value)
 
     def render_reset(self):
         self.rendered = []
 
 
     def render_field(self, name):
-        pass
+        
+        field_type, value = self.fields[name]
+
+        tpls = ["fields/%s.jinja" % (field_type,)]
+        tpls.append("fields/field.jinja")
+
+        self.rendered.append(name)
+        return render_template(tpls, field_type=field_type, value=value)
 
 
     def render_fields(self):
-        pass
+        
+        rendered_fields = u''
+
+        for name in self.fields.keys():
+
+            if name not in self.rendered:
+                rendered_fields += self.render_field(name)
+
+        return rendered_fields
