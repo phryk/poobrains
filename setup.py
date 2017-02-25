623620bfae98b49d0f046b65cbf935bdeaa9ad5f
phryk <phryk@commander1024.de>
2017-02-26 00:42:41 +0100

    trying setup.py foo

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='poobrains',
     author='phryk',
     install_requires=['flask', 'peewee', 'pyspkac', 'pyScss'],
+    extras_require={
+        'dev': ['pudb'], 
+    },
     packages=find_packages(),
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
