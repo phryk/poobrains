4b931c95aae1efc968d4aa1b6f5bf725ed9c7847
phryk <phryk@commander1024.de>
2017-02-25 23:37:29 +0100

    more setup.py stuff

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='poobrains',
+    author='phryk',
+    install_requires=['flask', 'peewee', 'pyspkac', 'pyScss'],
+    packages=['poobrains'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
     ]
