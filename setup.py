4c5379de4b5729833f7d64e6670230e2f1a3d029
phryk <phryk@commander1024.de>
2017-03-01 21:07:37 +0100

    more setup.py stuff

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='poobrains',
     author='phryk',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 2.7',
+    ],
     install_requires=['flask', 'peewee', 'pyspkac', 'pyScss'],
     extras_require={
         'dev': ['pudb'], 
     },
     packages=find_packages(),
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python :: 2.7',
-    ]
+    package_data = {
+        '': ['*.jinja', '*.scss', '*.svg'],
+    },
 )
