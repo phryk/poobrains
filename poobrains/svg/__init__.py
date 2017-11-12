960932dd41ea84edd184ef0be4161e493251fcdd
User Phryk <phryk@commander1024.de>
2017-11-12 04:07:34 +0100

    started work on map stuff

--- poobrains/svg/__init__.py
+++ poobrains/svg/__init__.py
@@ -3,6 +3,7 @@
 from poobrains import app, abort
 import poobrains.rendering
 import poobrains.storage
+import poobrains.tagging
 import poobrains.commenting
 
 class SVG(poobrains.rendering.Renderable):
@@ -142,3 +143,105 @@ class Plot(SVG):
     def label_y(self):
 
         return u' / '.join([dataset.label_y for dataset in self.datasets])
+
+
+class MapDatasetForm(poobrains.auth.AddForm):
+
+    def __init__(self, model_or_instance, **kwargs):
+
+        super(MapDatasetForm, self).__init__(model_or_instance, **kwargs)
+
+        n = 0
+        for datapoint in self.instance.datapoints:
+            
+            name = 'datapoint-%d' % n
+            setattr(self, name, poobrains.form.ProxyFieldset(datapoint.form('edit')))
+            n += 1
+
+        setattr(self, 'datapoint-add', poobrains.form.ProxyFieldset(MapDatapoint().form('add')))
+
+
+class MapDataset(poobrains.commenting.Commentable):
+
+    form_add = MapDatasetForm
+
+    title = poobrains.storage.fields.CharField()
+    description = poobrains.md.MarkdownField(null=True)
+
+
+#class MapDatapointFieldset(poobrains.form.Fieldset):
+#
+#    def __init__(self, datapoint, **kwargs):
+#
+#        super(MapDatapointFieldset, self).__init__(**kwargs)
+#
+#        self.datapoint = datapoint
+#        self.latitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.x, placeholder=MapDatapoint.latitude.verbose_name, help_text=MapDatapoint.latitude.help_text)
+#        self.longitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.y, placeholder=MapDatapoint.longitude.verbose_name, help_text=MapDatapoint.longitude.help_text)
+#
+#
+#    def process(self, submit, dataset):
+#
+#        if self.datapoint in dataset.datapoints:
+#            pass # florp
+#        else:
+#            self.datapoint.dataset = dataset
+#            self.datapoint.latitude = self.fields['latitude'].value
+#            self.datapoint.longitude = self.fields['longitude'].value
+#
+#            self.datapoint.save(force_insert=True)
+
+
+class MapDatapoint(poobrains.tagging.Taggable):
+
+    dataset = poobrains.storage.fields.ForeignKeyField(MapDataset, related_name='datapoints')
+    title = poobrains.storage.fields.CharField()
+    description = poobrains.md.MarkdownField(null=True)
+    latitude = poobrains.storage.fields.DoubleField()
+    longitude = poobrains.storage.fields.DoubleField()
+
+
+    # mercator calculation shamelessly thieved from the osm wiki
+    # http://wiki.openstreetmap.org/wiki/Mercator
+
+    @property
+    def x(self):
+      r_major=6378137.000
+      return r_major*math.radians(self.longitude)
+
+
+    @property
+    def y(self):
+      if self.latitude>89.5:self.lat=89.5
+      if self.latitude<-89.5:self.latitude=-89.5
+      r_major=6378137.000
+      r_minor=6356752.3142
+      temp=r_minor/r_major
+      eccent=math.sqrt(1-temp**2)
+      phi=math.radians(self.latitude)
+      sinphi=math.sin(phi)
+      con=eccent*sinphi
+      com=eccent/2
+      con=((1.0-con)/(1.0+con))**com
+      ts=math.tan((math.pi/2-phi)/2)/con
+      y=0-r_major*math.log(ts)
+      return y
+
+
+@app.expose('/svg/map')
+class Map(SVG):
+
+    datasets = None
+
+    def __init__(self, handle=None, mode=None, **kwargs):
+
+        super(Map, self).__init__(**kwargs)
+        self.datasets = []
+
+        if handle is None:
+            abort(404)
+
+        dataset_names = handle.split(',')
+
+        for name in dataset_names:
+            self.datasets.append(MapDataset.load(name))
