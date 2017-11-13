2d5609f3ebd1d7e90701e79064b9f16ffffb04f2
User Phryk <phryk@commander1024.de>
2017-11-13 05:26:10 +0100

    more map, more style

--- poobrains/svg/__init__.py
+++ poobrains/svg/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 from poobrains import app, abort
 import poobrains.rendering
 import poobrains.storage
@@ -155,10 +157,10 @@ class MapDatasetForm(poobrains.auth.AddForm):
         for datapoint in self.instance.datapoints:
             
             name = 'datapoint-%d' % n
-            setattr(self, name, poobrains.form.ProxyFieldset(datapoint.form('edit')))
+            setattr(self, name, MapDatapointFieldset(datapoint))
             n += 1
 
-        setattr(self, 'datapoint-add', poobrains.form.ProxyFieldset(MapDatapoint().form('add')))
+        setattr(self, 'datapoint-add', MapDatapointFieldset(MapDatapoint()))
 
 
 class MapDataset(poobrains.commenting.Commentable):
@@ -169,27 +171,33 @@ class MapDataset(poobrains.commenting.Commentable):
     description = poobrains.md.MarkdownField(null=True)
 
 
-#class MapDatapointFieldset(poobrains.form.Fieldset):
-#
-#    def __init__(self, datapoint, **kwargs):
-#
-#        super(MapDatapointFieldset, self).__init__(**kwargs)
-#
-#        self.datapoint = datapoint
-#        self.latitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.x, placeholder=MapDatapoint.latitude.verbose_name, help_text=MapDatapoint.latitude.help_text)
-#        self.longitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.y, placeholder=MapDatapoint.longitude.verbose_name, help_text=MapDatapoint.longitude.help_text)
-#
-#
-#    def process(self, submit, dataset):
-#
-#        if self.datapoint in dataset.datapoints:
-#            pass # florp
-#        else:
-#            self.datapoint.dataset = dataset
-#            self.datapoint.latitude = self.fields['latitude'].value
-#            self.datapoint.longitude = self.fields['longitude'].value
-#
-#            self.datapoint.save(force_insert=True)
+class MapDatapointFieldset(poobrains.form.Fieldset):
+
+    def __init__(self, datapoint, **kwargs):
+
+        super(MapDatapointFieldset, self).__init__(**kwargs)
+
+        self.datapoint = datapoint
+        self.name = poobrains.form.fields.Text(type=poobrains.form.types.STRING, value=datapoint.name, placeholder=MapDatapoint.name.verbose_name, help_text=MapDatapoint.name.help_text)
+        self.title = poobrains.form.fields.Text(type=poobrains.form.types.STRING, value=datapoint.title, placeholder=MapDatapoint.title.verbose_name, help_text=MapDatapoint.title.help_text)
+        self.latitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.x, placeholder=MapDatapoint.latitude.verbose_name, help_text=MapDatapoint.latitude.help_text)
+        self.longitude = poobrains.form.fields.Text(type=poobrains.form.types.FLOAT, value=self.datapoint.y, placeholder=MapDatapoint.longitude.verbose_name, help_text=MapDatapoint.longitude.help_text)
+        self.description = poobrains.form.fields.TextArea(placeholder=MapDatapoint.description.verbose_name, help_text=MapDatapoint.description.help_text)
+
+
+    def process(self, submit, dataset):
+
+        if self.datapoint in dataset.datapoints:
+            pass # florp
+        else:
+            self.datapoint.dataset = dataset
+
+            self.datapoint.name = self.fields['name'].value
+            self.datapoint.latitude = self.fields['latitude'].value
+            self.datapoint.longitude = self.fields['longitude'].value
+            self.datapoint.description = self.fields['description'].value
+
+            self.datapoint.save(force_insert=True)
 
 
 class MapDatapoint(poobrains.tagging.Taggable):
@@ -206,35 +214,41 @@ class MapDatapoint(poobrains.tagging.Taggable):
 
     @property
     def x(self):
-      r_major=6378137.000
-      return r_major*math.radians(self.longitude)
+
+        if not self.longitude is None:
+            r_major=6378137.000
+            return r_major*math.radians(self.longitude)
 
 
     @property
     def y(self):
-      if self.latitude>89.5:self.lat=89.5
-      if self.latitude<-89.5:self.latitude=-89.5
-      r_major=6378137.000
-      r_minor=6356752.3142
-      temp=r_minor/r_major
-      eccent=math.sqrt(1-temp**2)
-      phi=math.radians(self.latitude)
-      sinphi=math.sin(phi)
-      con=eccent*sinphi
-      com=eccent/2
-      con=((1.0-con)/(1.0+con))**com
-      ts=math.tan((math.pi/2-phi)/2)/con
-      y=0-r_major*math.log(ts)
-      return y
+
+        if not self.latitude is None:
+
+            if self.latitude>89.5:self.lat=89.5
+            if self.latitude<-89.5:self.latitude=-89.5
+            r_major=6378137.000
+            r_minor=6356752.3142
+            temp=r_minor/r_major
+            eccent=math.sqrt(1-temp**2)
+            phi=math.radians(self.latitude)
+            sinphi=math.sin(phi)
+            con=eccent*sinphi
+            com=eccent/2
+            con=((1.0-con)/(1.0+con))**com
+            ts=math.tan((math.pi/2-phi)/2)/con
+            y=0-r_major*math.log(ts)
+            return y
 
 
 @app.expose('/svg/map')
 class Map(SVG):
 
+    handle = None # needed so that app.expose registers a route with extra param, this is kinda hacky…
     datasets = None
 
     def __init__(self, handle=None, mode=None, **kwargs):
-
+        app.debugger.set_trace()
         super(Map, self).__init__(**kwargs)
         self.datasets = []
 
